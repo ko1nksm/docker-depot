572abb716c9b4df2136f0bf6341d2e8c1bfa7919
Koichi Nakashima <koichi@nksm.name>
2015-12-11 18:07:39 +0900

    Workaround when bash-completion is too old

--- completions/docker-depot.bash
+++ completions/docker-depot.bash
@@ -1,10 +1,19 @@
 _docker-depot() {
+  local fallback=""
+  type _get_comp_words_by_ref >/dev/null 2>&1 || fallback=1
+  type __ltrim_colon_completions >/dev/null 2>&1 || fallback=1
+
   local cur
-  _get_comp_words_by_ref -n : cur
+  local line=${COMP_WORDS[@]:1:$COMP_CWORD-1}
 
-  COMPREPLY=( $( compgen -W "$(docker-depot completions ${COMP_WORDS[@]:1:$COMP_CWORD-1})" -- "$cur") )
-
-  __ltrim_colon_completions "$cur"
+  if [[ $fallback ]]; then
+    cur=${COMP_WORDS[$COMP_CWORD]}
+    COMPREPLY=( $( compgen -W "$(docker-depot completions $line)" -- "$cur") )
+  else
+    _get_comp_words_by_ref -n : cur
+    COMPREPLY=( $( compgen -W "$(docker-depot completions $line)" -- "$cur") )
+    __ltrim_colon_completions "$cur"
+  fi
 }
 
 complete -F _docker-depot docker-depot
